7fc81b6e4bf2c657e8afc51a3216c1fe704dfe3e
XinyuanYan <1779929499@qq.com>
2022-10-03 19:17:17 -0600

    new

---
+++ app/static/js/global.js
@@ -0,0 +1,37 @@
+/**
+ * Process all button click event
+ */
+
+// press the 'Start Survey' button
+function indexBtnListener(){
+    alert('ok');
+    console.log('ojjk');
+    d3.select('form').node().submit();
+
+
+
+
+    // location.replace("../static/templates/bg.html");
+
+    // axios.post('/startSurvey', {
+    //     firstName: 'Fred',
+    //     lastName: 'Flintstone'
+    //   })
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+
+    // 可选地，上面的请求可以这样做.  
+// axios.post('/material', {
+//     firstName: 'Fred'
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+}
--- app/views.py
+++ app/views.py
@@ -1,6 +1,40 @@
 from app import app
-from flask import render_template
+from flask import Flask, render_template, request, flash
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/background', methods=['POST', 'GET'])
+def backgroundTest():
+    return render_template('bg.html')
+
+@app.route('/pretestIntro', methods=['POST', 'GET'])
+def pretestIntro():
+    return render_template('pretestIntro.html')
+
+@app.route('/pretest', methods=['POST', 'GET'])
+def pretest():
+    return render_template('pretest.html')
+
+@app.route('/material', methods=['POST', 'GET'])
+def material():
+    # print(request.form)
+    # return flash("you are successfuly logged in")
     return render_template('material1.html')
+
+@app.route('/posttestIntro', methods=['POST', 'GET'])
+def posttestIntro():
+    return render_template('posttestIntro.html')
+
+@app.route('/posttest-1', methods=['POST', 'GET'])
+def posttest_1():
+    return render_template('posttest_1.html')
+
+@app.route('/posttest-2', methods=['POST', 'GET'])
+def posttest_2():
+    return render_template('posttest_2.html')
+
+@app.route('/engagement', methods=['POST', 'GET'])
+def engagement():
+    return render_template('engage.html')
